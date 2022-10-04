0b028fa432af2d5892fd8bb726995ff1d09c1a1f
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-10-03 21:49:21 -0500

    Inital commit

---
+++ KiwiToys/KiwiToys/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using KiwiToys.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KiwiToys.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
