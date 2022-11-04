8ab651642e7c9b7102ebd8ff1e243d201903d993
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-11-04 09:42:49 -0600

    Resources for the web page added

---
+++ KiwiToys/KiwiToys/wwwroot/js/DropdownCascade.js
@@ -0,0 +1,48 @@
+﻿const dropdownCascade = (urlGetStates, urlGetCities) => {
+    $("#CountryId").change(function () {
+        $("#StateId").empty();
+        $("#StateId").append('<option value="0">[Selecciona un Departamento / Estado...]</option>');
+        $("#CityId").empty();
+        $("#CityId").append('<option value="0">[Selecciona una ciudad...]</option>');
+
+        $.ajax({
+            type: 'POST',
+            url: urlGetStates,
+            dataType: 'json',
+            data: { countryId: $("#CountryId").val() },
+            success: function (states) {
+                $.each(states, function (i, state) {
+                    $("#StateId").append('<option value="' + state.id + '">' + state.name + '</option>');
+                });
+            },
+            error: function (ex) {
+                alert('Failed to retrieve states.' + ex);
+            }
+        });
+
+        return false;
+    });
+
+    $("#StateId").change(function () {
+        $("#CityId").empty();
+        $("#CityId").append('<option value="0">[Selecciona una ciudad...]</option>');
+
+        $.ajax({
+            type: 'POST',
+            url: urlGetCities,
+            dataType: 'json',
+            data: { stateId: $("#StateId").val() },
+            success: function (cities) {
+                $.each(cities, function (i, city) {
+                    debugger;
+                    $("#CityId").append('<option value="' + city.id + '">' + city.name + '</option>');
+                });
+            },
+            error: function (ex) {
+                alert('Failed to retrieve cities.' + ex);
+            }
+        });
+
+        return false;
+    });
+}
---
+++ KiwiToys/KiwiToys/wwwroot/js/showmodal.js
@@ -0,0 +1,75 @@
+﻿showInPopup = (url, title) => {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (res) {
+            $('#form-modal .modal-body').html(res);
+            $('#form-modal .modal-title').html(title);
+            $('#form-modal').modal('show');
+        }
+    });
+}
+
+jQueryAjaxPost = form => {
+    try {
+        $.ajax({
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.isValid) {
+                    $('#view-all').html(res.html)
+                    $('#form-modal .modal-body').html('');
+                    $('#form-modal .modal-title').html('');
+                    $('#form-modal').modal('hide');
+                    // reload the table
+                    location.reload()
+                } else {
+                    $('#form-modal .modal-body').html(res.html);
+                }
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        });
+
+        //to prevent default form submit event
+        return false;
+    } catch (ex) {
+        console.log(ex)
+    }
+}
+
+(function (soccerDeleteDialog) {
+    var methods = {
+        "openModal": openModal,
+        "deleteItem": deleteItem
+    };
+
+    var item_to_delete;
+
+    function openModal(modalName, classOrId, sourceEvent, deletePath, eventClassOrId) {
+        var textEvent = classOrId
+            ? "." + modalName
+            : "#" + modalName;
+
+        $(textEvent).click((e) => {
+            item_to_delete = e.currentTarget.dataset.id;
+            deleteItem(sourceEvent, deletePath, eventClassOrId);
+        });
+    }
+
+    function deleteItem(sourceEvent, deletePath, eventClassOrId) {
+        var textEvent = eventClassOrId
+            ? "." + sourceEvent
+            : "#" + sourceEvent;
+
+        $(textEvent).click(function () {
+            window.location.href = deletePath + item_to_delete;
+        });
+    }
+
+    soccerDeleteDialog.sc_deleteDialog = methods;
+})(window);
